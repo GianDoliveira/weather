1455522648cae6a026dc2337ca89e4a93394ec1a
Gian de Oliveira <naigdeoliveira@gmail.com>
2022-11-30 15:22:05 -0200

    axios

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [local, setLocal] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${local}&units=metric&lang=pt_br&appid=b02166061d90cfd41cfa2444a6be2809`
+
+  const search =(event)=>{
+    if (event.key === 'Enter') {
+      axios.get(url)
+      .then((response)=> {
+        setData(response.data)
+      })
+      setLocal('')
+    }
+  }
+
   return (
       <div>
         
