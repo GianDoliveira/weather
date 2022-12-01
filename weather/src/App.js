ae506f5e3297a6fdaaaa57121615b0783d841080
Gian de Oliveira <naigdeoliveira@gmail.com>
2022-12-01 16:59:16 -0200

    script

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,29 +1,59 @@
 import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import {BsFillDropletFill} from 'react-icons/bs'
+import {SiWindicss} from 'react-icons/si'
 
-function App() {
+
+export default function App() {
 
   const [data, setData] = useState({})
   const [local, setLocal] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${local}&units=metric&lang=pt_br&appid=b02166061d90cfd41cfa2444a6be2809`
+  const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${local}&units=metric&lang=pt_br&appid=b02166061d90cfd41cfa2444a6be2809`;
 
-  const search =(event)=>{
-    if (event.key === 'Enter') {
-      axios.get(url)
-      .then((response)=> {
-        setData(response.data)
-      })
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(urlWeather)
+        .then((response) => {
+          setData(response.data)
+        })
       setLocal('')
     }
   }
-
   return (
-      <div>
-        
+    <div className='app'>
+      <div className="search">
+        <input
+          value={local}
+          onChange={e => setLocal(e.target.value)}
+          onKeyPress={search}
+          type="text"
+          placeholder='Pesquisar'
+        />
       </div>
+
+      <div className='container'>
+        <div className='city'>
+          Rio de janeiro, BR
+        </div>
+        <div className='temperature'>
+          15°C
+        </div>
+        <div className='weather'>Clounds</div>
+        <div className='details'>
+          <p className='humidity'>
+            <i><BsFillDropletFill/></i>
+            <span>90%</span>
+          </p>
+          <p className='wind'>
+            <i><SiWindicss/></i>
+            <span>9km/h</span>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App;
